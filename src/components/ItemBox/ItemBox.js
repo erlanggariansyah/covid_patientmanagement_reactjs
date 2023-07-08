f934b644eadcf5a4e69e9c969d1b6ff69138353b
anggaifg <erlanggariansyah0@gmail.com>
2023-07-09 05:57:07 +0700

    Update Grid Layout Vercel Error

--- src/components/ItemBox/ItemBox.js
+++ src/components/ItemBox/ItemBox.js
@@ -3,21 +3,23 @@ import styles from "./ItemBox.module.css"
 const ItemBox = (props) => {
     const { container, title, subtitle } = styles;
     const { titleText, subtitleText } = props;
-    const conditionalColor = {
-        color: 'black'
-    }
+    const conditionalColor = { color: 'black' }
+    let titleTextFinal;
 
     if (titleText === "confirmed") {
         conditionalColor.color = "#06D6A0"
+        titleTextFinal = "Confirmed"
     } else if (titleText === "recovered") {
         conditionalColor.color = "#118AB2"
+        titleTextFinal = "Recovered"
     } else {
         conditionalColor.color = "red"
+        titleTextFinal = "Death"
     }
 
     return (
         <div className={container}>
-            <p className={title}>{titleText}</p>
+            <p className={title}>{titleTextFinal}</p>
             <p className={subtitle} style={conditionalColor}>{subtitleText.toLocaleString()}</p>
         </div>
     )
